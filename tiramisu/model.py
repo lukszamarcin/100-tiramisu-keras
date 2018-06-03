01059d4aa607bf111469474944975b5b9de5017d
xxmarl <marcin.luksza@gmail.com>
2018-06-03 19:06:08 +0200

    update model

--- tiramisu/model.py
+++ tiramisu/model.py
@@ -2,49 +2,37 @@ from keras.layers import *
 from keras.regularizers import l2
 
 
-#TODO: it is not neccesaary like this - shorten it
-def relu(x): return Activation('relu')(x)
-
-
-def dropout(x, p): return Dropout(p)(x) if p else x
-
-
-def bn(x): return BatchNormalization(axis=-1)(x)
-
-
-def relu_bn(x): return relu(bn(x))
-
-
-def concat(xs): return Concatenate(axis=-1)(xs)
+def relu_bn(x): return Activation('relu')(BatchNormalization(axis=-1)(x))
 
 
 def conv(x, nf, sz, wd, p, stride=1):
     x = Conv2D(nf, (sz, sz), strides=(stride, stride), padding='same', kernel_initializer='he_uniform',
-               kernel_regularizer = l2(wd))(x)
-    return dropout(x, p)
+               kernel_regularizer=l2(wd))(x)
+    return Dropout(p)(x) if p else x
 
 
 def conv_relu_bn(x, nf, sz=3, wd=0, p=0, stride=1):
     return conv(relu_bn(x), nf, sz, wd=wd, p=p, stride=stride)
 
 
-def dense_block(n,x,growth_rate,p,wd):
+def dense_block(n, x, growth_rate, p, wd):
     added = []
     for i in range(n):
         b = conv_relu_bn(x, growth_rate, p=p, wd=wd)
-        x = concat([x, b])
+        x = Concatenate(axis=-1)([x, b])
         added.append(b)
     return x, added
 
 
 def transition_dn(x, p, wd):
+    # in the paper stride=1 but better results with stride=2
     return conv_relu_bn(x, x.get_shape().as_list()[-1], sz=1, p=p, wd=wd, stride=2)
 
 
 def down_path(x, nb_layers, growth_rate, p, wd):
     skips = []
-    for i,n in enumerate(nb_layers):
-        x,added = dense_block(n,x,growth_rate,p,wd)
+    for i, n in enumerate(nb_layers):
+        x, added = dense_block(n, x, growth_rate, p, wd)
 
         # keep track of skip connections
         skips.append(x)
@@ -53,19 +41,19 @@ def down_path(x, nb_layers, growth_rate, p, wd):
 
 
 def transition_up(added, wd=0):
-    x = concat(added)
-    _,r,c,ch = x.get_shape().as_list()
+    x = Concatenate(axis=-1)(added)
+    _, r, c, ch = x.get_shape().as_list()
     return Conv2DTranspose(ch, (3, 3), strides=(2, 2), padding='same', kernel_initializer='he_uniform',
                  kernel_regularizer = regularizers.l2(wd))(x)
 
 
 def up_path(added, skips, nb_layers, growth_rate, p, wd):
     # use previously saved list of skip connections
-    for i,n in enumerate(nb_layers):
+    for i, n in enumerate(nb_layers):
         x = transition_up(added, wd)
 
         # concatenate the skip connections
-        x = concat([x,skips[i]])
+        x = Concatenate(axis=-1)([x, skips[i]])
         x, added = dense_block(n, x, growth_rate, p, wd)
     return x
 
@@ -75,6 +63,7 @@ def reverse(a): return list(reversed(a))
 
 def create_tiramisu(nb_classes, img_input, nb_dense_block=6,
                     growth_rate=16, nb_filter=48, nb_layers_per_block=[4, 5, 7, 10, 12, 15], p=0.2, wd=1e-4):
+
     if type(nb_layers_per_block) is list or type(nb_layers_per_block) is tuple:
         nb_layers = list(nb_layers_per_block)
     else:
